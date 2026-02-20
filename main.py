0cf15a984d7f7cf47682b5a21bd1b594075f5111
Nicolas Carraro <e12202357@student.tuwien.ac.at>
2026-02-20 10:30:04 +0100

    third prototype

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+os.environ["QT_SCALE_FACTOR"] = "1"
+os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "0"
 from src.ui.windows.main_window import run_gui
 
 import src.ui.resources_rc  # type: ignore
